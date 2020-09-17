f17aba8fd575e20b2fecef75cfbb8b42111beef2
cynwachi <christinenwachi@gmail.com>
2020-09-16 22:59:20 -0500

    changed html

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// I want a function that will first listen for events 
 
+function operands() {
+ // Grab the buttons and kmow the value of the buttons
+}
+function(calculation) {
+
+}
+
+//When any of these 16 buttons are clicked the thing clicked will display 
+
+
+
+// When = is pushed the 2 numbers will be operated on
+
+
+
+//When clear is pushed the display will be cleared 
