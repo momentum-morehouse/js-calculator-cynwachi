6b442cd3cd548af8de790a2adcf2c2058f4e394e
cynwachi <christinenwachi@gmail.com>
2020-09-17 01:35:46 -0500

    javascript not connected

---
+++ calculator.js
@@ -0,0 +1,13 @@
+const display = document.querySelector(".calculator__display");
+//global variables go here
+function input () {
+    //grabbing buttons 
+    const buttons = document.querySelectorAll(".js-button");
+
+    buttons.addEventListener("click", function(){
+        //anonymous function 
+        display.innerHTML= buttons.value;
+    });
+}
+// call function 
+input();
