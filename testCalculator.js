955c61f3bdf294d850cc761c8b0de80750b7594d
cynwachi <christinenwachi@gmail.com>
2020-09-21 02:43:37 -0500

    new calculator

--- testCalculator.js
+++ testCalculator.js
@@ -1,12 +1,39 @@
-console.log("test")
-let buttons = document.querySelectorAll (".buttons")
+function calc() {
+    // makes browser make me write good JS
+    "use strict";
 
-for (let button of buttons){
-    //console.log(buttons)
-    //console.log(7)
-    button.addEventListener("click", function (event){
-        //holds function part 
-        console.log(event.target) // referring to variables that refer to other things
+    const display = document.querySelector('.displayOut');
+    const keys = document.querySelectorAll('.buttons');
+    let input = [];
+    let expression= "";
+    for (const key of keys) {
+        key.addEventListener('click', function () {
+            if (this.classList.contains('operand')){
+                display.value += this.value;
+            }
+            if (this.classList.contains('operant')) {
+                input.push(parseFloat(display.value));
+                input.push(this.value);
+                display.value = "";
+            }
+            if (this.classList.contains('clearButton')){
+                input = [];
+                display.value = "";
+            }
+            if (this.classList.contains('equalButton')) {
+                input.push(parseFloat(display.value));
+                
+                for (let x = 0; x < input.length; x++) {
+                    expression += input[x];
+                }
+
+                display.value = eval(expression);
+                input =[];
+                expression = "";
+            }
+        });
+        
+        
     }
-    )
 }
+calc();
